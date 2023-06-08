2133f68a007f57e4fba2889bafd9db879562d155
MichelleL8157 <98603675+MichelleL8157@users.noreply.github.com>
2023-06-08 13:21:15 -0400

    Add files via upload
    Added comments

--- js.js
+++ js.js
@@ -1,6 +1,11 @@
+/*
+When the 'Guess' button is pressed, the function will check if the user inputs "1000000" which is
+the correct value. If the value is correct, there will be a new message on the web page about
+the hint and if not, no information is given.
+*/
 function guessed(){
     var answer = document.getElementById("text").value;
-    if (answer = "1000000") {
+    if (answer.includes(1000000)) {
         document.getElementById("resp").textContent = "Look at our hints page!";
     } else {
         document.getElementById("resp").textContent = "You imposter!";
