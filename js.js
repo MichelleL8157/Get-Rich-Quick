c3389f4ac3f975544fd976e12b37d300554ed7d1
MichelleL8157 <98603675+MichelleL8157@users.noreply.github.com>
2023-06-09 19:28:59 -0400

    Add files via upload

--- js.js
+++ js.js
@@ -1,12 +1,11 @@
-/*
-When the 'Guess' button is pressed, the function will check if the user inputs "1000000" which is
-the correct value. If the value is correct, there will be a new message on the web page about
-the hint and if not, no information is given.
-*/
+// When the 'Guess' button is pressed,
 function guessed(){
     var answer = document.getElementById("text").value;
+    // the function will check if the user inputs "1000000"
     if (answer.includes(1000000)) {
+        //if so, there will be a new message on the web page about the hint
         document.getElementById("resp").textContent = "Look at our hints page!";
+    //if not, no information is given.
     } else {
         document.getElementById("resp").textContent = "You imposter!";
     }
